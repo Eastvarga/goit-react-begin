f13f8ed7f8f4bd9ed50e65aef63b171f8e722b94
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-03-20 20:09:48 +0200

    Updates

---
+++ static/js/main.9482c93b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-begin"]=this["webpackJsonpgoit-react-begin"]||[]).push([[0],{9:function(e,t,n){"use strict";n.r(t);n(1);var c=n(3),i=n.n(c),r=n(0),o=function(){return Object(r.jsx)("div",{children:Object(r.jsx)("h1",{children:"Main component"})})};i.a.render(Object(r.jsx)(o,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.9482c93b.chunk.js.map
