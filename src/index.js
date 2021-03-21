920b7bc7378ab2f717b5b5f3855b6730779e3e12
Alexander Serzhenko <alexsergspectr@gmail.com>
2021-03-21 11:06:32 +0200

    first commit install react

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// import './index.css';
+
+// import reportWebVitals from './reportWebVitals';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+// const link = React.createElement("a", { href: "reactjs.org" }, "ссылочка");
+// console.log(link);
+
+// const jsxLink = <a href="react.org">Ссылочка</a>;
+// console.log(jsxLink);
+
+// ReactDOM.render(jsxLink, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
